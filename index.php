3b9410fd48e380a178df72c2e1a0828cdeeaff43
alexandre <alexandre.aboujaib@telecom-paris.fr>
2026-07-02 21:26:19 +0200

    experimenting

--- index.php
+++ index.php
@@ -2,6 +2,18 @@
 <head>
     <link rel="stylesheet" href="res/css/theCSS.css">
     <title>Minesweeper</title>
+    <script>
+        function updateBoard(x,y) {
+            var xmlhttp = new XMLHttpRequest();
+            xmlhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    document.getElementById("txtHint").innerHTML = "hellow";
+                }
+            };
+            xmlhttp.open("GET", "index.php?x="+x+"&y="+y, true);
+            xmlhttp.send();
+        }
+    </script>
 </head>
 <html>
     <body>
@@ -12,9 +24,12 @@
             // require -> erreur si le fichier exsiste ps
             // _once -> charge ps le fichier si il est déjà chargé
             // $_SERVER['DOCUMENT_ROOT'] -> Racine du site
-            // . -> concaténtion
+            // . -> concaténation
 
             require_once $_SERVER['DOCUMENT_ROOT'] . 'src/Game/GameManager.php';
+
+            $xu = $_REQUEST["x"];
+            $yu = $_REQUEST["y"];
 
             use App\Game\GameManager;
 
@@ -27,7 +42,7 @@
                 for ($i = 0; $i < $size; $i++) {
                     echo "<div>";
                     for ($j = 0; $j < $size; $j++) {
-                        echo "<span class=\"cell\" id=" . $gameManager->getDiv($i,$j) . ">";
+                        echo "<span onclick=\"updateBoard(3,3);\" class=\"cell\" id=" . $gameManager->getDiv($i,$j) . ">";
                         echo $gameManager->getAtXY($i,$j);
                         echo "</span>";
                     }
@@ -38,9 +53,13 @@
             refresh($gameManager,$size);
 
             echo "\n";
-            echo "--------------------------";
+            echo "-------------------------- ";
+            echo $xu;
+            echo " | ";
+            echo $yu;
 
         ?>
         </div>
+        <span id="txtHint">hey</span>
     </body>
 </html>
